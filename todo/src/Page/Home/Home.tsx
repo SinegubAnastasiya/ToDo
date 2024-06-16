ae2c02ec351a9fa38fdd86e7f201e58a5f91ca21
SinegubAnastasiya <fedukevich98@mail.ru>
2024-06-16 21:37:27 +0300

    added Home.tsx

--- todo/src/Page/Home/Home.tsx
+++ todo/src/Page/Home/Home.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+
 function Home() {
-    return ( 
-        <>
-        </> 
-    );
+    const [task, setTask] = useState([
+        {
+        id: 1,
+        todo: "Do something nice for someone you care about",
+        completed: false,
+        userId: 152
+        },
+        {
+        id: 2,
+        todo: "Memorize a poem",
+        completed: true,
+        userId: 13
+        },
+        {
+        id: 3,
+        todo: "Watch a classic movie",
+        completed: true,
+        userId: 68
+        }])
+
+    const deleteTask = (id: number) => setTask(task.filter((el) => el.id !== id))
+
+    return <>
+        {task.map((el, i) => <div key={i}>{el.todo} <button onClick={() => deleteTask(el.id)}>Delete</button></div>)}
+    </> 
 }
 
 export default Home;
